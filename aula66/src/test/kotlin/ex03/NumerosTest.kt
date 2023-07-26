830e9d51e2aa2efb00e7677888ecb67ce780b81a
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-26 10:42:49 -0300

    enviando atividade

--- aula66/src/test/kotlin/ex03/NumerosTest.kt
+++ aula66/src/test/kotlin/ex03/NumerosTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions.*
 class NumerosTest {
 
     @Test
-    fun informarMaiorNumero() {
+    fun deveInformarMaiorNumero() {
         val numeros = Numeros()
         val listaNumeros = listOf(1,10,10000,100,1000)
         assertEquals(10000, numeros.informarMaiorNumero(listaNumeros))
